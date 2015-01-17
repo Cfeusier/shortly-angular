08ccd19d61df9656c38d18c8d19f56bd735699af
Clark Feusier <cfeusier@gmail.com>
2015-01-16 16:17:29 -0800

    Finish all auth and validations

--- client/app/app.js
+++ client/app/app.js
@@ -24,7 +24,7 @@ angular.module('shortly', [
       controller: 'ShortenController'
     })
     .otherwise({
-      redirectTo:'/links'
+      redirectTo: '/links'
     })
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
@@ -56,7 +56,7 @@ angular.module('shortly', [
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+    if (next.$$route && !Auth.isAuth()) {
       $location.path('/signin');
     }
   });
